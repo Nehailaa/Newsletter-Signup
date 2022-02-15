39f3f5d6de33eb5f9009420a02dfdada9abec69c
Nehailaa <93628865+Nehailaa@users.noreply.github.com>
2022-02-14 19:21:05 -0600

    status code condition

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
 const request = require("request");
+const { dir } = require("console");
+const { dirname } = require("path/posix");
 
 const app = express();
 // for public folder that contains static css & images
@@ -41,6 +43,13 @@ app.post("/", (req, res) => {
     };
 
    const request=  https.request(url, options, response => {
+
+    if(response.statusCode === 200){
+        res.sendFile(__dirname + "/success.html");
+    }else{
+        res.sendFile(__dirname + "/failure.html");
+    };
+
         response.on("data", data => {
             console.log(JSON.parse(data));
         });
@@ -49,6 +58,11 @@ app.post("/", (req, res) => {
     request.end();
 
 });
+
+app.post("/failure", (req, res) =>{
+      res.redirect("/");
+});
+
 
 app.listen(3000, () => {
     console.log(`Server is running on port 3000.`);
